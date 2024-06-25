6ced5fd45a153a8c84a169f31e1f7399e251612f
yoseidonn <yk.yusuf.2006@gmail.com>
2024-06-25 10:42:01 +0300

    Searchbox duzeltme

--- main.py
+++ main.py
@@ -27,6 +27,8 @@ class MainWindow(QMainWindow):
         
         self.st_index = 0
         self.last_clicked_item = QListWidgetItem()
+        self.villas = list()
+        self.one_cikarilan_items = list()
         
         # Villa arama işlemi için thread
         self.search_thread = VillaSearchThread()
@@ -83,11 +85,6 @@ class MainWindow(QMainWindow):
                 self.ozellikler_widget.addItem(item)
                 self.ozellik_items.append(item)
         
-        # Search Line Edit Engine
-        self.popup_list.setFrameStyle(QFrame.Panel | QFrame.Raised)
-        self.popup_list.setWindowFlags(Qt.Popup)
-        self.popup_list.hide()
-        
         # Yükleniyor animasyonu
         self.loading_label = QLabel(self)
         self.loading_movie = QMovie("loading.gif")
@@ -96,13 +93,17 @@ class MainWindow(QMainWindow):
         self.loading_label.hide()
         self.main_layout.addWidget(self.loading_label)
         
+        # Seçilen villaların gösterileceği liste - self.selected_villas
+        self.selected_villas.setSelectionMode(QListWidget.NoSelection)
+        self.selected_villas.mousePressEvent = self.on_villa_item_click
+        
     def set_signals(self):
         self.previous_btn.clicked.connect(self.st_prev_btn_clicked)
         self.next_btn.clicked.connect(self.st_next_btn_clicked)
 
         # -> Widgets
-        self.bolgeler_widget.itemClicked.connect(lambda item: self.item_clicked(item))
-        self.ozellikler_widget.itemClicked.connect(lambda item: self.item_clicked(item))
+        self.bolgeler_widget.itemClicked.connect(lambda item: self.st_item_clicked(item))
+        self.ozellikler_widget.itemClicked.connect(lambda item: self.st_item_clicked(item))
         
         self.select_all1.clicked.connect(lambda: self.change_state(self.bolge_items, Qt.Checked))
         self.select_all2.clicked.connect(lambda: self.change_state(self.ozellik_items, Qt.Checked))
@@ -110,21 +111,6 @@ class MainWindow(QMainWindow):
         self.deselect_all2.clicked.connect(lambda: self.change_state(self.ozellik_items, Qt.Unchecked))
 
         self.search_button.clicked.connect(self.start_search)
-        
-        # -> Search Line Edit Engine
-        self.search_box.textChanged.connect(self.on_text_changed)
-        self.popup_list.itemClicked.connect(self.on_item_clicked)
-
-        # Yükleniyor animasyonu
-        self.loading_label = QLabel(self)
-        self.loading_movie = QMovie("loading.gif")
-        self.loading_label.setMovie(self.loading_movie)
-        self.loading_label.setAlignment(Qt.AlignCenter)
-        self.loading_label.hide()
-        self.main_layout.addWidget(self.loading_label)
-        
-        self.next_btn.hide()
-        self.previous_btn.hide()
         
     def start_search(self):
         self.search_frame.hide()
@@ -135,7 +121,6 @@ class MainWindow(QMainWindow):
         self.search_thread.start()
     
     def on_search_finished(self, villas):
-        print(f"Villas: {villas}")
         self.villas = villas
         self.loading_movie.stop()
         self.loading_label.hide()
@@ -143,29 +128,15 @@ class MainWindow(QMainWindow):
         self.previous_btn.show()
         self.st_index = 1
 
-    def on_text_changed(self):
-        # Arama kutusu değiştikçe çağrılır
-        text = self.search_box.text().lower()
-        if text:
-            print(f"Text: {text}")
-            self.popup_list.clear()
-            filtered_villas = [villa if text in villa.lower() else None for villa in self.villas]
-            print(f"Filtered villas: {filtered_villas}")
-            if filtered_villas:
-                self.popup_list.addItems(filtered_villas)
-                self.popup_list.show()
-                self.popup_list.raise_()  # Açılır listeyi en üstte göster
-            else:
-                self.popup_list.hide()
-        else:
-            self.popup_list.hide()
-            
-    def on_item_clicked(self, item):
-        # Bir öğeye tıklanıldığında çağrılır
-        self.selected_villas.addItem(QListWidgetItem(item.text()))
-        self.popup_list.hide()
-        self.search_box.clear()
-    
+        # QCompleter oluştur
+        self.completer = QCompleter(self.villas, self)
+        self.completer.setCaseSensitivity(Qt.CaseInsensitive)
+        self.completer.setFilterMode(Qt.MatchContains)
+        self.search_box.setCompleter(self.completer)
+        
+        # QCompleter'daki öğeye tıklanınca öğeyi seçilen villalar listesine ekle
+        self.completer.activated.connect(self.add_selected_villa)
+        
     def st_prev_btn_clicked(self):
         if self.st_index:
             self.result_frame.hide()
@@ -182,18 +153,35 @@ class MainWindow(QMainWindow):
             self.next_btn.hide()
             self.previous_btn.show()
             
-    def item_clicked(self, item: QListWidgetItem):
+    def st_item_clicked(self, item: QListWidgetItem):
         if not item.checkState():
             item.setCheckState(Qt.Checked)
         else:
             item.setCheckState(Qt.Unchecked)
         self.last_clicked_item = item
 
+    def on_villa_item_click(self, event):
+        # Tiklanan ogeyi bul ve sil
+        item = self.selected_villas.itemAt(event.pos())
+        if item:
+            self.selected_villas.takeItem(self.selected_villas.row(item))
+
     def change_state(self, items, stateTo):
         for item in items:
             item.setCheckState(stateTo)
         self.last_clicked_item.setSelected(False)
         self.last_clicked_item = QListWidgetItem()
+    
+    def add_selected_villa(self, text):
+        item = QListWidgetItem(text)
+        self.one_cikarilan_items.append(item)
+        self.selected_villas.addItem(item)
+        self.search_box.clear()
+        
+        self.update_text()
+
+    def update_text(self):
+        pass
     
     def set_window_settings(self):
         self.setWindowTitle("Project Alpha | EKLENTI PAKETI")
